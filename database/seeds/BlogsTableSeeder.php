3904418e34b3e17f378bba2f416fc255fe39ba0a
naoki <duvingtdeux@gmail.com>
2019-12-13 13:15:09 +0900

    Blogs用のシード作成

--- database/seeds/BlogsTableSeeder.php
+++ database/seeds/BlogsTableSeeder.php
@@ -11,6 +11,20 @@ class BlogsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Clear Table
+        DB::table('blogs')->truncate();
+
+        // シードデータの作成
+        $blogs = [
+                  ['title' => 'はじめての投稿',
+                   'article' => 'はじめての投稿です。よろしくお願いします'],
+                  ['title' => '2つ目の投稿',
+                   'article' => '色々なことを書いていきたいと思います。毎日少しずつ積み重ねて日記を書いていきますね']
+                  ];
+
+        // シードの登録
+        foreach($blogs as $blog) {
+            \App\Blog::create($blog);
+        }
     }
 }
