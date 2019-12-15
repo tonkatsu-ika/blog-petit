79a09c3c084f42e431fed37a6e7445731681143e
naoki <duvingtdeux@gmail.com>
2019-12-15 10:03:09 +0900

    基本的なindex/edit actionを作成

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Blog;
 
 class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+      $blogs = Blog::all();
+      return view('blog/index', compact('blogs'));
+    }
+
+    public function edit($id)
+    {
+      $blog = Blog::findOrFail($id);
+      return view('blog/edit', compact('book'));
+    }
 }
