af14146c8bdb89f9d8aaf9fd82e6b012fc9247db
naoki <duvingtdeux@gmail.com>
2019-12-15 22:18:18 +0900

    storeとupdate用のフォームテンプレート作成

---
+++ resources/views/blog/form.blade.php
@@ -0,0 +1,33 @@
+<div class="container ops-main">
+  <div class="row">
+    <div class="col-md-6">
+      @if($target == 'store')
+      <h2>投稿画面</h2>
+      @else
+      <h2>編集画面</h2>
+      @endif
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-8 col-md-offset-1">
+      @if($target == 'store')
+      <form action="/blog" method="post">
+      @elseif($target == 'update')
+      <form action="/blog/{{ $blog->id }}" method="post">
+        <input type="hidden" name="_method" value="PUT">
+      @endif
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="form-group">
+          <label for="title">ブログのタイトル</label>
+          <input type="text" class="form-control" name="title" value="{{ $blog->title }}">
+        </div>
+        <div class="form-group">
+          <label for="article">記事の内容</label>
+          <textarea class="form-control" name="article" rows="5">{{ $blog->article }}</textarea>
+        </div>
+        <button type="submit" class="btn btn-default">更新</button>
+        <a href="/blog">一覧画面へ戻る</a>
+      </form>
+    </div>
+  </div>
+</div>
