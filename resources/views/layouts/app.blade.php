3ba4591cd1f7c4913f0dc9c0543a3f53ff8fff30
naoki <duvingtdeux@gmail.com>
2019-12-15 10:13:50 +0900

    app共通のレイアウトファイルを作成

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>Blog Petit|@yeild('title')</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
