4ba70b754f40fe9ac4a2dd92264dee900412e9de
naoki <duvingtdeux@gmail.com>
2019-12-19 16:35:01 +0900

    認証用のページを日本語化

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+                <div class="card-header">{{ __('パスワードの確認') }}</div>
 
                 <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+                    {{ __('パスワードをご確認のうえ先へお進みください'}}
 
                     <form method="POST" action="{{ route('password.confirm') }}">
                         @csrf
@@ -30,7 +30,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
+                                    {{ __('パスワードを確認する') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
